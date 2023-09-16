8252e311fea4ff939c1243b43d12421ea123f5f2
bugradogann <ahmetbugra_94@hotmail.com>
2023-09-16 14:49:15 -0400

    Hooks class added where setUp and tearDown happens before each test scenario.

---
+++ src/test/java/lexisNexisAssesment/step_definitions/Hooks.java
@@ -0,0 +1,31 @@
+package lexisNexisAssesment.step_definitions;
+
+import lexisNexisAssesment.utilities.ConfigurationReader;
+import lexisNexisAssesment.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().manage().window().maximize();
+        Driver.get().get(ConfigurationReader.get("url"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        //takes screenshot if the scenario is failed and adds it into report under target file
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+
+
+}
