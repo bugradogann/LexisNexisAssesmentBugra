908a1677f067cb6fb515c77462df38460425e79a
bugradogann <ahmetbugra_94@hotmail.com>
2023-09-16 14:46:15 -0400

    BrowserUtils class added where some ready methods exist.

---
+++ src/test/java/lexisNexisAssesment/utilities/BrowserUtils.java
@@ -0,0 +1,116 @@
+package lexisNexisAssesment.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class BrowserUtils {
+
+    /**
+     Switches to new window by the exact title. Returns to original window if target title not found
+     */
+    public static void switchToWindow(String targetTitle) {
+        String origin = Driver.get().getWindowHandle();
+        for (String handle : Driver.get().getWindowHandles()) {
+            Driver.get().switchTo().window(handle);
+            if (Driver.get().getTitle().equals(targetTitle)) {
+                return;
+            }
+        }
+        Driver.get().switchTo().window(origin);
+    }
+
+
+    /**
+     Moves the mouse to given element
+     */
+    public static void hover(WebElement element) {
+        Actions actions = new Actions(Driver.get());
+        actions.moveToElement(element).perform();
+    }
+
+
+    /**
+     Performs a pause
+     */
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     Verifies whether the element is displayed on page
+     */
+    public static void verifyElementDisplayed(WebElement element) {
+        try {
+            Assert.assertTrue("Element not visible: " + element, element.isDisplayed());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            Assert.fail("Element not found: " + element);
+        }
+    }
+
+
+    /**
+     Scrolls down to an element using JavaScript
+     */
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    /**
+     Performs double click action on an element
+     */
+    public static void doubleClick(WebElement element) {
+        new Actions(Driver.get()).doubleClick(element).build().perform();
+    }
+
+
+    /**
+     Highlights an element by changing its background and border color
+     */
+    public static void highlight(WebElement element) {
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+        waitFor(1);
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].removeAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+    }
+
+
+    /**
+     Checks or unchecks given checkbox
+     */
+    public static void selectCheckBox(WebElement element, boolean check) {
+        if (check) {
+            if (!element.isSelected()) {
+                element.click();
+            }
+        } else {
+            if (element.isSelected()) {
+                element.click();
+            }
+        }
+    }
+
+
+    /**
+     attempts to click on provided element until given time runs out
+     */
+    public static void clickWithTimeOut(WebElement element, int timeout) {
+        for (int i = 0; i < timeout; i++) {
+            try {
+                element.click();
+                return;
+            } catch (WebDriverException e) {
+                waitFor(1);
+            }
+        }
+    }
+
+}
